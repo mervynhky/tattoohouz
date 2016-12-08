54346a6509b5bb57c5ca165c50219cc311aaeb22
mervynhky <mervyn93hky@gmail.com>
2016-12-08 11:40:28 +0800

    Added b2t and subpage

--- js/tattoo.js
+++ js/tattoo.js
@@ -1,3 +1,23 @@
+//BACK-TO-TOP
+$('body').prepend('<a href="#" class="back-to-top"></a>');
+
+var amountScrolled = 300;
+
+$(window).scroll(function() {
+    if ( $(window).scrollTop() > amountScrolled ) {
+        $('a.back-to-top').fadeIn('slow');
+    } else {
+        $('a.back-to-top').fadeOut('slow');
+    }
+});
+
+$('a.back-to-top').click(function() {
+    $('html, body').animate({
+        scrollTop: 0
+    }, 700);
+    return false;
+});
+
 //BUTTONS: INDEX PAGE
 
 function homePage(){
